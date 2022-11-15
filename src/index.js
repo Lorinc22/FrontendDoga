82674a0fa2ead43a5b8f9c1b8225cf65838a2794
Krity Lorinc <krity.lorinc@gmail.com>
2022-11-15 14:46:00 +0100

    felkesz

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
-import './style.css';
+document.addEventListener('DOMContentLoaded'), () =>{
 
+    //1.Feladat Összes Idézet + Szerző nevek
+    document.getElementById('osszes').addEventListener('click', async ()=>{
+        let response = await fetch('/users.json');
+        let data = await response.json();
+        data.quotes.map(quote => quotes.author.toUpperCase()+','+ quote.quotes);
+        quotes.sort((a,b) =>a.localeCompare(b));
+        let quoteLista = document.getElementById('lista')
+        for(let u of quotes){
+            let li = document.createElement('li');
+            li.textContent=u;
+            quoteLista.appendChild(li);
+        }
+    });
+    //2.Feladat The
+    document.getElementById('the').addEventListener('click', async ()=>{
+        let response = await fetch('/users.json');
+        let data = await response.json();
+
+    });
+    //3.Feladat Hossz
+    document.getElementById('hossz').addEventListener('click', async ()=>{
+        let response = await fetch('/users.json');
+        let data = await response.json();
+
+    });
+    //4.feladat Darabszám
+    document.getElementById('darabszam').addEventListener('click', async ()=>{
+        let response = await fetch('/users.json');
+        let data = await response.json();
+
+    });
+
+    
+}
